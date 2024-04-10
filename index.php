4bf3c0c6d030d1e213f85ddb8bf578f0e7824a8a
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-10 14:22:29 +0800

    Update CSS styles for navigation and main content

--- index.php
+++ index.php
@@ -1,54 +1,56 @@
-<?php
-session_start();
-?>
+<main?php session_start(); ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-    <link rel="stylesheet" href="static/css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+        <link rel="stylesheet" href="static/css/style.css">
+    </head>
 
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="index.php">Home</a>
-                </li>
-                <?php
-                if (isset($_SESSION['useruid'])) {
-                    echo '<li>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <?php
+                    if (isset($_SESSION['useruid'])) {
+                        echo '<li>
                             <a href="includes/logout.include.php">Logout</a>
                         </li>';
-                } else {
-                    echo '<li>
+                    } else {
+                        echo '<li>
                             <a href="signup.php">Sign Up</a>
                         </li>';
-                    echo '<li>
+                        echo '<li>
                             <a href="login.php">Log In</a>
                         </li>';
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section>
+                <h1>Hello World</h1>
+                <?php
+                if (isset($_SESSION["useruid"])) {
+                    echo "<p>Hello there " . $_SESSION["useruid"] . "</p>";
+                } else {
+                    echo "<p>You are not logged in</p>";
                 }
                 ?>
-            </ul>
-        </nav>
-    </header>
-    <h1>Hello World</h1>
-    <?php
-    if (isset($_SESSION["useruid"])) {
-        echo "<p>Hello there " . $_SESSION["useruid"] . "</p>";
-    } else {
-        echo "<p>You are not logged in</p>";
-    }
-    ?>
-    <footer>
-        <p>&copy;
-            <?php echo date('Y'); ?> - All Rights Reserved
-        </p>
-    </footer>
-</body>
+            </section>
+        </main>
+        <footer>
+            <p>&copy;
+                <?php echo date('Y'); ?> - All Rights Reserved
+            </p>
+        </footer>
+    </body>
 
-</html>
+    </html>
--- login.php
+++ login.php
@@ -1,63 +1,63 @@
-<?php
-session_start();
-?>
+<secti?php session_start(); ?>
 
-<!DOCTYPE html>
-<html lang="en">
+    <!DOCTYPE html>
+    <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log In</title>
-    <link rel="stylesheet" href="static/css/style.css">
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Log In</title>
+        <link rel="stylesheet" href="static/css/style.css">
+    </head>
 
-<body>
-    <header>
-        <nav>
-            <ul>
-                <li>
-                    <a href="index.php">Home</a>
-                </li>
-                <?php
-                if (isset($_SESSION['useruid'])) {
-                    echo '<li>
+    <body>
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <?php
+                    if (isset($_SESSION['useruid'])) {
+                        echo '<li>
                             <a href="includes/logout.include.php">Logout</a>
                         </li>';
-                } else {
-                    echo '<li>
+                    } else {
+                        echo '<li>
                             <a href="signup.php">Sign Up</a>
                         </li>';
-                    echo '<li>
+                        echo '<li>
                             <a href="login.php">Log In</a>
                         </li>';
+                    }
+                    ?>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <section>
+                <h2>Log In</h2>
+                <form action="includes/login.include.php" method="post">
+                    <label for="name">Name</label>
+                    <input type="text" name="uid" id="name" placeholder="username" required><br>
+                    <label for="pwd">Password</label>
+                    <input type="password" name="pwd" id="pwd" required><br>
+                    <button type="submit" name="submit">Log In</button>
+                </form>
+                <?php
+                if (isset($_GET["error"])) {
+                    if ($_GET["error"] == "wronglogin") {
+                        echo "<p>Incorrect login information!</p>";
+                    }
                 }
                 ?>
-            </ul>
-        </nav>
-    </header>
-    <main>
-        <h2>Log In</h2>
-        <form action="includes/login.include.php" method="post">
-            <label for="name">Name</label>
-            <input type="text" name="uid" id="name" placeholder="username" required><br>
-            <label for="pwd">Password</label>
-            <input type="password" name="pwd" id="pwd" required><br>
-            <button type="submit" name="submit">Log In</button>
-        </form>
-        <?php
-        if (isset($_GET["error"])) {
-            if ($_GET["error"] == "wronglogin") {
-                echo "<p>Incorrect login information!</p>";
-            }
-        }
-        ?>
-    </main>
-    <footer>
-        <p>&copy;
-            <?php echo date('Y'); ?> - All Rights Reserved
-        </p>
-    </footer>
-</body>
+            </section>
+        </main>
+        <footer>
+            <p>&copy;
+                <?php echo date('Y'); ?> - All Rights Reserved
+            </p>
+        </footer>
+    </body>
 
-</html>
+    </html>
