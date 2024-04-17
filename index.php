bbedfa5ff1979806099ad39504e9cbc41b955a07
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-17 19:48:47 +0800

    Update index.php to display user information when logged in

--- index.php
+++ index.php
@@ -18,10 +18,20 @@ session_start();
         <section>
             <h1>Simple Log In and Sign Up System</h1>
             <?php
-            if (isset($_SESSION["users_username"])) {
+            if (isset($_SESSION["users_id"])) {
             ?>
                 <p>Welcome back <?php echo $_SESSION["users_username"] ?>!</p>
                 <p>You are currently logged in using <?php echo $_SESSION["users_email"] ?>.</p>
+                <form action="#">
+                    <label for="firstName">First Name</label>
+                    <input type="text" name="firstName" id="firstName" value='<?php echo $_SESSION["users_first_name"] ?>' readonly><br>
+                    <label for="lastName">Last Name</label>
+                    <input type="text" name="lastName" id="lastName" value='<?php echo $_SESSION["users_last_name"] ?>' readonly><br>
+                    <label for="username">Username</label>
+                    <input type="text" name="username" id="username" value='<?php echo $_SESSION["users_username"] ?>' readonly><br>
+                    <label for="email">Email</label>
+                    <input type="email" name="email" id="email" value='<?php echo $_SESSION["users_email"] ?>' readonly><br>
+                </form>
             <?php
             } else {
             ?>
