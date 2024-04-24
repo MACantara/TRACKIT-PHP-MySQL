bb5056d17bb9fd1e3fbad585bda25a4b7a9201e8
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-24 21:00:24 +0800

    Refactor password reset functionality and include necessary files

--- classes/PasswordResetRequestController.class.php
+++ classes/PasswordResetRequestController.class.php
@@ -2,6 +2,7 @@
 
 require_once 'PasswordResetRequestView.class.php';
 require_once 'PasswordResetRequest.class.php';
+require_once '../config.php';
 class PasswordResetRequestController extends PasswordResetRequest {
     public function handleRequest($userEmail) {
         $passwordResetRequest = new PasswordResetRequest();
@@ -10,7 +11,7 @@ class PasswordResetRequestController extends PasswordResetRequest {
 
         $selector = bin2hex(random_bytes(8));
         $token = random_bytes(32);
-        $url = "localhost/TRACKIT-PHP-MySQL/create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
+        $url = BASE_URL . "create-new-password.php?selector=" . $selector . "&validator=" . bin2hex($token);
         $expires = date("U") + 1800;
         $hashedToken = password_hash($token, PASSWORD_DEFAULT);
 
