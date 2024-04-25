c147338043bb01c6fc8a26de58fd5c781127bead
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-26 03:20:11 +0800

    Add JavaScript code to handle category selection in add-transaction.js

---
+++ static/add-transaction.js
@@ -0,0 +1,7 @@
+document.getElementById('transaction_category').addEventListener('change', function () {
+    if (this.value === 'other') {
+        document.getElementById('new_transaction_category').style.display = 'block';
+    } else {
+        document.getElementById('new_transaction_category').style.display = 'none';
+    }
+});
