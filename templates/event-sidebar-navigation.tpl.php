94428a30f4bd32faf947400e66c64e82ef533c72
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-25 23:47:19 +0800

    Remove event-graph.php, event-sidebar-navigation.tpl.php, and transaction-history.php files

--- templates/event-sidebar-navigation.tpl.php
+++
@@ -1,8 +0,0 @@
-<aside>
-    <ul>
-        <li><a href="events-overview.php">Back to Events Overview</a></li>
-        <li><a href="event-dashboard.php?events_id=<?php echo $eventId; ?>">Event Dashboard</a></li>
-        <li><a href="transaction-history.php?events_id=<?php echo $eventId; ?>">Transaction History</a></li>
-        <li><a href="report-generation.php?events_id=<?php echo $eventId; ?>">Report Generation</a></li>
-    </ul>
-</aside>
