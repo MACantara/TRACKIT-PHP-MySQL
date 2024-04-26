3853606bfea0a2e4c59450f7139d71648629bb5f
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-27 00:46:05 +0800

    Update update-event.inc.php to include event time in updateEvent function

--- includes/update-event.inc.php
+++ includes/update-event.inc.php
@@ -2,14 +2,15 @@
 
 require_once 'event-functions.inc.php';
 
-if (isset($_POST['update-event'])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update-event'])) {
     $events_id = $_POST['events_id'];
     $events_name = $_POST['event_name'];
     $events_description = $_POST['event_description'];
     $events_date = $_POST['event_date'];
+    $events_time = $_POST['event_time']; // Retrieve event time from form data
     $events_budget = $_POST['event_budget'];
 
-    updateEvent($conn, $events_id, $events_name, $events_description, $events_date, $events_budget);
+    updateEvent($conn, $events_id, $events_name, $events_description, $events_date, $events_time, $events_budget); // Pass event time to updateEvent function
 
     header("Location: ../events-overview.php?event-updated=success");
 }
