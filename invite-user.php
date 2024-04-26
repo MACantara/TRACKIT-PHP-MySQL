dc01928d00821259d929033bca179e62d9a68814
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-26 23:38:38 +0800

    Refactor invite-user.php to update section class and button layout

--- invite-user.php
+++ invite-user.php
@@ -18,14 +18,17 @@ $row = getEvent($conn, $eventId);
 <body>
     <?php include 'templates/header.tpl.php'; ?>
     <main>
-        <section>
-            <h1>Invite a User to Manage <?php echo $row['events_name']; ?></h1>
-            <p>Enter the email address of the user you would like to invite.</p>
-            <form action="includes/invite-user.inc.php" method="post">
+        <section class="section-container">
+            <h1>Invite a User</h1>
+            <p class="margin-top-16">Enter the email address of the user you would like to invite to help you manage <?php echo $row['events_name']; ?>.</p>
+            <form class="margin-top-16" action="includes/invite-user.inc.php" method="post">
                 <label for="email">Email</label>
                 <input type="email" name="email" id="email" placeholder="Enter your email" required><br>
                 <input type="hidden" name="events_id" value="<?php echo $eventId; ?>">
-                <button class="button" type="submit" name="Send Invite">Send Invite</button>
+                <div class="two-grid-column-container">
+                    <a class="button" href="event-dashboard.php?events_id=<?php echo $eventId; ?>">Back</a>
+                    <button class="button" type="submit" name="Send Invite">Send Invite</button>
+                </div>
             </form>
             <?php
             if (isset($_GET["reset"])) {
