320ae248bede6f1d0fad257952abc0f7fd4d63da
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-27 01:08:48 +0800

    Add input validation to createEvent function in event.inc.php

--- includes/event.inc.php
+++ includes/event.inc.php
@@ -1,8 +1,39 @@
 <?php
 require_once 'db-connection.inc.php';
+require_once 'error-handling-functions.inc.php';
 
 
 function createEvent($conn, $userId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) {
+    if (eventEmptyInput($eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) !== false) {
+        header("location: create-event.php?error=emptyinput");
+        exit();
+    }
+
+    if (eventNameExists($conn, $eventName) !== false) {
+        header("location: create-event.php?error=eventnametaken");
+        exit();
+    }
+
+    if (tooLongEventName($eventName) !== false) {
+        header("location: create-event.php?error=toolongeventname");
+        exit();
+    }
+
+    if (invalidEventDate($eventDate) !== false) {
+        header("location: create-event.php?error=invalideventdate");
+        exit();
+    }
+
+    if (invalidEventTime($eventTime) !== false) {
+        header("location: create-event.php?error=invalideventtime");
+        exit();
+    }
+
+    if (invalidEventBudget($eventBudget) !== false) {
+        header("location: create-event.php?error=invalideventbudget");
+        exit();
+    }
+
     $eventDateTime = $eventDate . 'T' . $eventTime . ':00';
     $sql = "INSERT INTO events (events_name, events_description, events_date, events_budget) VALUES (?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
