9e7f8a11d7bb3c8c82f1a589617a08b4e6b69d06
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-26 23:38:19 +0800

    Refactor events-overview.php to update section class and button layout

--- events-overview.php
+++ events-overview.php
@@ -20,10 +20,10 @@ $events = getUserEvents($conn, $userId);
 <body>
     <?php include 'templates/header.tpl.php'; ?>
     <main>
-        <section>
+        <section class="section-container">
             <h1>Events Overview</h1>
             <div class="button-container">
-                <a class="button" href="create-event.php">Create New Event</a>
+                <a class="button margin-top-16" href="create-event.php">Create New Event</a>
             </div>
         </section>
         <div class="events-grid">
@@ -32,8 +32,17 @@ $events = getUserEvents($conn, $userId);
                     <div>
                         <h2><a href="event-dashboard.php?events_id=<?php echo $event['events_id']; ?>"><?php echo $event['events_name']; ?></a></h2>
                         <p><?php echo $event['events_description']; ?></p>
-                        <p>Date: <?php echo $event['events_date']; ?></p>
-                        <p>Budget: <?php echo $event['events_budget']; ?></p>
+                        <p class="margin-top-16">Date: <?php echo $event['events_date']; ?></p>
+                        <p>Budget: &#8369; <?php echo number_format($event['events_budget'], 2); ?></p>
+                        <div class="grid-container">
+                            <a class="button button-primary margin-top-16" href="event-dashboard.php?events_id=<?php echo $event['events_id']; ?>">View</a>
+                            <a class="button secondary-outline-button margin-top-16" href="update-event.php?events_id=<?php echo $event['events_id']; ?>">Update</a>
+                            <a class="button secondary-outline-button margin-top-16" href="invite-user.php?events_id=<?php echo $event['events_id']; ?>">Invite</a>
+                        </div>
+                        <form method="post" action="includes/delete-event.inc.php">
+                            <input type="hidden" name="events_id" value="<?php echo $event['events_id']; ?>">
+                            <button type="submit" class="button button-outline-danger margin-top-16">Delete</button>
+                        </form>
                     </div>
                 </section>
             <?php endforeach; ?>
