1de3dc99534d03f81b8f5cd4183c6315e8d14dc3
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-27 00:46:00 +0800

    Add event time input to createEvent function in event.inc.php

--- includes/event.inc.php
+++ includes/event.inc.php
@@ -1,10 +1,12 @@
 <?php
 require_once 'db-connection.inc.php';
 
-function createEvent($conn, $userId, $eventName, $eventDescription, $eventDate, $eventBudget) {
+
+function createEvent($conn, $userId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) {
+    $eventDateTime = $eventDate . 'T' . $eventTime . ':00';
     $sql = "INSERT INTO events (events_name, events_description, events_date, events_budget) VALUES (?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "ssss", $eventName, $eventDescription, $eventDate, $eventBudget);
+    mysqli_stmt_bind_param($stmt, "ssss", $eventName, $eventDescription, $eventDateTime, $eventBudget);
     mysqli_stmt_execute($stmt);
     $eventId = mysqli_insert_id($conn);
 
