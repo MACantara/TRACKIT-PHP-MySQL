9bcfbb0e381eefe928daba4b2f5dff9008f27f75
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-26 16:33:26 +0800

    Update event-dashboard.php to format transaction amounts and prices

--- event-dashboard.php
+++ event-dashboard.php
@@ -65,9 +65,9 @@ $transactions = getTransactions($conn, $eventId);
                     <tr>
                         <td><?php echo $transaction['transaction_date']; ?></td>
                         <td><?php echo $transaction['transaction_name']; ?></td>
-                        <td><?php echo $transaction['transaction_amount']; ?></td>
-                        <td><?php echo $transaction['transaction_price']; ?></td>
-                        <td><?php echo $transaction['transaction_total']; ?></td>
+                        <td><?php echo number_format($transaction['transaction_amount']); ?></td>
+                        <td><?php echo 'PHP ' . number_format($transaction['transaction_price'], 2); ?></td>
+                        <td><?php echo 'PHP ' . number_format($transaction['transaction_total'], 2); ?></td>
                         <td><?php echo $transaction['transaction_category']; ?></td>
                         <td><?php echo $transaction['transaction_type']; ?></td>
                         <td><?php echo $transaction['transaction_description']; ?></td>
@@ -77,76 +77,7 @@ $transactions = getTransactions($conn, $eventId);
         </section>
     </main>
     <?php include 'templates/footer.tpl.php'; ?>
-    <script>
-        // Pie chart 1
-        const pieChart1Ctx = document.getElementById('pieChart1').getContext('2d');
-        new Chart(pieChart1Ctx, {
-            type: 'pie',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow'],
-                datasets: [{
-                    data: [300, 50, 100],
-                    backgroundColor: ['rgb(255, 99, 132)', 'rgb(54, 162, 235)', 'rgb(255, 205, 86)']
-                }]
-            },
-            options: {
-                responsive: true,
-            }
-        });
-        
-        // Pie chart 2
-        const pieChart2Ctx = document.getElementById('pieChart2').getContext('2d');
-        new Chart(pieChart2Ctx, {
-            type: 'pie',
-            data: {
-                labels: ['Green', 'Purple', 'Orange'],
-                datasets: [{
-                    data: [200, 150, 50],
-                    backgroundColor: ['rgb(75, 192, 192)', 'rgb(153, 102, 255)', 'rgb(255, 159, 64)']
-                }]
-            },
-            options: {
-                responsive: true,
-            }
-        });
-        
-        // Bar chart
-        const barChartCtx = document.getElementById('barChart').getContext('2d');
-        new Chart(barChartCtx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
+    <script src="static/js/event-dashboard-charts.js"></script>
 </body>
 
 </html>
