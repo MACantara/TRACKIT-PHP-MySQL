27d4f186f968177a5e8ef6e3e5b29480d5bbe614
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-27 01:53:33 +0800

    Add input validation and error handling to reset-request.inc.php

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -1,10 +1,21 @@
 <?php
 require_once "../config.php";
 require_once "error-handling-functions.inc.php";
+require_once '../includes/forgot-password-functions.inc.php';
 
 if (isset($_POST['reset-request-submit'])) {
     $userEmail = $_POST["email"];
-    require_once '../includes/forgot-password-functions.inc.php';
+
+    if (passwordRequestEmptyInput($userEmail) || invalidEmail($userEmail)) {
+        header("Location: ../forgot-password.php?error=emptyinput");
+        exit();
+    }
+
+    if (!emailNotFound($userEmail, $conn)) {
+        header("Location: ../forgot-password.php?error=emailnotfound");
+        exit();
+    }
+
     handleRequest($conn, $userEmail);
     header("location: ../forgot-password.php?reset=success");
 } else {
