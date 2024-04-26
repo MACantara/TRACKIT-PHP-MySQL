0a08948b6488254fd6db43123a2c19a9d15e2aee
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-04-26 16:33:38 +0800

    Add report generation functions in report-generation-functions.inc.php

---
+++ includes/report-generation-functions.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+function formatNumber($num) {
+    if($num > 1000) {
+        $x = round($num);
+        $x_number_format = number_format($x);
+        $x_array = explode(',', $x_number_format);
+        $x_parts = array('K', 'M', 'B', 'T');
+        $x_count_parts = count($x_array) - 1;
+        $x_display = $x;
+        $x_display = $x_array[0] . ((int) $x_array[1][0] !== 0 ? '.' . $x_array[1][0] : '');
+        $x_display .= $x_parts[$x_count_parts - 1];
+        return 'PHP ' . $x_display;
+    }
+    return 'PHP ' . number_format($num, 2);
+}
