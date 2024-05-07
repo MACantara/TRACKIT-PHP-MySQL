04d28272677693893f6aad8380b62ee8c55dd0ec
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2024-05-07 20:43:30 +0800

    Refactor password reset request to use user ID instead of email

--- includes/reset-request.inc.php
+++ includes/reset-request.inc.php
@@ -6,15 +6,17 @@ require_once '../includes/forgot-password-functions.inc.php';
 if (isset($_POST['reset-request-submit'])) {
     $userEmail = $_POST["email"];
 
-    // Check if password reset request for the email already exists
-    $sql = "SELECT * FROM password_reset WHERE password_reset_email = ?";
+    // Check if password reset request for the user already exists
+    $sql = "SELECT * FROM password_reset 
+            JOIN users ON password_reset.password_reset_users_id = users.users_id 
+            WHERE users.users_email = ?";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("s", $userEmail);
     $stmt->execute();
     $result = $stmt->get_result();
 
     if ($result->num_rows > 0) {
-        // Password reset request for the email already exists
+        // Password reset request for the user already exists
         header("Location: ../forgot-password.php?error=passwordresetrequestalreadyexists");
         exit();
     }
