f20ce3f126e7928a485a654736b37fac53df3ad4
Michael Angelo R. Cantara <cantara.michaelangelo@gmail.com>
2024-05-07 13:14:03 +0800

    chore: Update header navigation links with icons for improved user experience

--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -6,28 +6,28 @@
         </a>
         <ul>
             <li>
-                <a href="index.php">Home</a>
+                <a href="index.php"><i class="bi bi-house-door"></i> Home</a>
             </li>
             <?php
             if (isset($_SESSION['users_id'])) {
                 ?>
                 <li>
-                    <a href="events-overview.php">Events Overview</a>
+                    <a href="events-overview.php"><i class="bi bi-calendar2-week"></i> Events Overview</a>
                 </li>
                 <li>
-                    <a href="profile-information.php"><?php echo $_SESSION["users_username"]; ?></a>
+                    <a href="profile-information.php"><i class="bi bi-person"></i> <?php echo $_SESSION["users_username"]; ?></a>
                 </li>
                 <li>
-                    <a href="includes/log-out.inc.php">Logout</a>
+                    <a href="includes/log-out.inc.php"><i class="bi bi-box-arrow-right"></i> Logout</a>
                 </li>
                 <?php
             } else {
                 ?>
                 <li>
-                    <a href="sign-up.php">Sign Up</a>
+                    <a href="sign-up.php"><i class="bi bi-person-plus"></i> Sign Up</a>
                 </li>
                 <li>
-                    <a href="log-in.php">Log In</a>
+                    <a href="log-in.php"><i class="bi bi-box-arrow-in-right"></i> Log In</a>
                 </li>
                 <?php
             }
