83ee5ae5ebd8ced04ea124c418387e0456165a13
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-09 01:06:16 +0800

    chore: Refactor profile information settings page and related functions

--- includes/profile-information.inc.php
+++ includes/profile-information.inc.php
@@ -9,13 +9,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $firstName = htmlspecialchars($_POST['firstName'], ENT_QUOTES, 'UTF-8');
     $lastName = htmlspecialchars($_POST['lastName'], ENT_QUOTES, 'UTF-8');
     $username = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
-    $email = htmlspecialchars($_POST['email'], ENT_QUOTES, 'UTF-8');
     $profileAbout = htmlspecialchars($_POST['profileAbout'], ENT_QUOTES, 'UTF-8');
     $profileTitle = htmlspecialchars($_POST['profileTitle'], ENT_QUOTES, 'UTF-8');
     $profileText = htmlspecialchars($_POST['profileText'], ENT_QUOTES, 'UTF-8');
+    $currentPassword = $_POST['currentPassword'];
 
-    updateUserInformation($conn, $firstName, $lastName, $username, $email, $users_id);
+    $userData = getUserIdInformationById($conn, $users_id);
+    $passwordHashed = $userData["users_password"];
+    $checkPassword = password_verify($currentPassword, $passwordHashed);
+
+    if ($checkPassword === false) {
+        header("location: ../profile-information-settings.php?error=wrongpassword");
+        exit();
+    }
+
+    updateUserInformation($conn, $firstName, $lastName, $username, $users_id);
     updateProfileInformation($conn, $profileAbout, $profileTitle, $profileText, $users_id);
 
-    header("location: ../profile-information.php?error=none");
+    header("location: ../profile-information-settings.php?error=none");
 }
