966e6c7b5d766dec8b0fc919450c544305a3131c
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-08 13:57:25 +0800

    Refactor variable names and update events_id in update-event.inc.php

--- includes/update-event.inc.php
+++ includes/update-event.inc.php
@@ -3,14 +3,14 @@
 require_once 'event-functions.inc.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update-event'])) {
-    $events_id = $_POST['events_id'];
-    $events_name = $_POST['event_name'];
-    $events_description = $_POST['event_description'];
-    $events_date = $_POST['event_date'];
-    $events_time = $_POST['event_time']; // Retrieve event time from form data
-    $events_budget = $_POST['event_budget'];
+    $eventsId = $_POST['events_id'];
+    $eventsName = $_POST['event_name'];
+    $eventsDescription = $_POST['event_description'];
+    $eventsDate = $_POST['event_date'];
+    $eventsTime = $_POST['event_time']; // Retrieve event time from form data
+    $eventsBudget = $_POST['event_budget'];
 
-    updateEvent($conn, $events_id, $events_name, $events_description, $events_date, $events_time, $events_budget); // Pass event time to updateEvent function
+    updateEvent($conn, $eventsId, $eventsName, $eventsDescription, $eventsDate, $eventsTime, $eventsBudget); // Pass event time to updateEvent function
 
     header("Location: ../events-overview.php?event-updated=success");
 }
