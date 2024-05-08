8aa511649b9cd178690f4622e054847654e0db10
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-08 13:56:58 +0800

    Refactor variable names and update events_id in multiple files

--- includes/event-functions.inc.php
+++ includes/event-functions.inc.php
@@ -4,14 +4,14 @@ require_once 'event.inc.php';
 function handleCreateEvent($conn) {
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         if (isset($_POST['event_name'], $_POST['event_description'], $_POST['event_date'], $_POST['event_time'], $_POST['event_budget'])) {
-            $userId = $_SESSION['users_id'];
+            $usersId = $_SESSION['users_id'];
             $eventName = $_POST['event_name'];
             $eventDescription = $_POST['event_description'];
             $eventDate = $_POST['event_date'];
             $eventTime = $_POST['event_time'];
             $eventBudget = $_POST['event_budget'];
 
-            createEvent($conn, $userId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget);
+            createEvent($conn, $usersId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget);
             header("Location: events-overview.php?create-event=success");
         } else {
             header("Location: events-overview.php?create-event=error");
@@ -19,28 +19,28 @@ function handleCreateEvent($conn) {
     }
 }
 
-function getEvent($conn, $eventId) {
+function getEvent($conn, $eventsId) {
     $sql = "SELECT * FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     return mysqli_fetch_assoc($result);
 }
 
-function getEventName($conn, $eventId) {
+function getEventName($conn, $eventsId) {
     $sql = "SELECT events_name FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     return $row['events_name'];
 }
 
-function getTransactions($conn, $eventId, $sort = 'DESC', $filterDays = null, $transactionType = null, $startFrom = 0, $recordsPerPage = 0) {
+function getTransactions($conn, $eventsId, $sort = 'DESC', $filterDays = null, $transactionType = null, $startFrom = 0, $recordsPerPage = 0) {
     $sql = "SELECT * FROM transaction_history WHERE events_id = ?";
-    $params = [$eventId];
+    $params = [$eventsId];
     $types = "i";
 
     if ($filterDays) {
@@ -67,58 +67,58 @@ function getTransactions($conn, $eventId, $sort = 'DESC', $filterDays = null, $t
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function getUserEvents($conn, $userId) {
+function getUserEvents($conn, $usersId) {
     $sql = "SELECT events.events_id, events.events_name, events.events_description, events.events_date, events.events_budget FROM events JOIN event_users ON events.events_id = event_users.events_id WHERE event_users.users_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $userId);
+    mysqli_stmt_bind_param($stmt, "i", $usersId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function getCategories($conn, $eventId) {
+function getCategories($conn, $eventsId) {
     $sql = "SELECT transaction_category FROM transaction_history WHERE events_id = ? GROUP BY transaction_category;"; // adjust the table and column names as needed
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         header("Location: ../index.php?error=sqlerror");
         exit();
     } else {
-        mysqli_stmt_bind_param($stmt, "i", $eventId);
+        mysqli_stmt_bind_param($stmt, "i", $eventsId);
         mysqli_stmt_execute($stmt);
         $result = mysqli_stmt_get_result($stmt);
         return mysqli_fetch_all($result, MYSQLI_ASSOC);
     }
 }
 
-function getTotalEventExpenses($conn, $eventId) {
+function getTotalEventExpenses($conn, $eventsId) {
     $sql = "SELECT SUM(transaction_total) AS total_expenses FROM transaction_history WHERE events_id = ? AND transaction_type = 'expense'";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     return $row['total_expenses'];
 }
 
-function getTotalEventIncome($conn, $eventId) {
+function getTotalEventIncome($conn, $eventsId) {
     $sql = "SELECT SUM(transaction_total) AS total_income FROM transaction_history WHERE events_id = ? AND transaction_type = 'income'";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
     return $row['total_income'];
 }
     
-function getEventRemainingBudget($conn, $eventId) {
-    $event = getEvent($conn, $eventId);
-    $expenses = getTotalEventExpenses($conn, $eventId);
-    $income = getTotalEventIncome($conn, $eventId);
+function getEventRemainingBudget($conn, $eventsId) {
+    $event = getEvent($conn, $eventsId);
+    $expenses = getTotalEventExpenses($conn, $eventsId);
+    $income = getTotalEventIncome($conn, $eventsId);
     $remainingBudget = ($event['events_budget'] + $income) - $expenses;
     return $remainingBudget;
 }
 
-function getEventManagers($conn, $eventId) {
+function getEventManagers($conn, $eventsId) {
     $sql = "SELECT users.users_last_name, users.users_first_name FROM event_users
             INNER JOIN users ON event_users.users_id = users.users_id
             WHERE event_users.events_id = ?";
@@ -126,7 +126,7 @@ function getEventManagers($conn, $eventId) {
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         return false;
     }
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $managers = array();
@@ -136,7 +136,7 @@ function getEventManagers($conn, $eventId) {
     return $managers;
 }
 
-function getEventExpenses($conn, $eventId)
+function getEventExpenses($conn, $eventsId)
 {
     $sql = "SELECT transaction_category, SUM(transaction_total) as transaction_total
             FROM transaction_history
@@ -148,7 +148,7 @@ function getEventExpenses($conn, $eventId)
         exit();
     }
 
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
 
     $resultData = mysqli_stmt_get_result($stmt);
@@ -159,7 +159,7 @@ function getEventExpenses($conn, $eventId)
     return $expenses;
 }
 
-function getEventIncomes($conn, $eventId) {
+function getEventIncomes($conn, $eventsId) {
     $sql = "SELECT transaction_category, SUM(transaction_total) as transaction_total
             FROM transaction_history
             WHERE events_id = ? AND transaction_type = 'Income'
@@ -170,7 +170,7 @@ function getEventIncomes($conn, $eventId) {
         exit();
     }
 
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
 
     $resultData = mysqli_stmt_get_result($stmt);
@@ -181,26 +181,26 @@ function getEventIncomes($conn, $eventId) {
     return $income;
 }
 
-function deleteEvent($conn, $eventId) {
+function deleteEvent($conn, $eventsId) {
     $sql = "DELETE FROM events WHERE events_id = ?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
         return false;
     }
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     return true;
 }
 
-function updateEvent($conn, $eventId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) {
+function updateEvent($conn, $eventsId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) {
     $eventDateTime = $eventDate . ' ' . $eventTime . ':00';
     $sql = "UPDATE events SET events_name = ?, events_description = ?, events_date = ?, events_budget = ? WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "ssssi", $eventName, $eventDescription, $eventDateTime, $eventBudget, $eventId);
+    mysqli_stmt_bind_param($stmt, "ssssi", $eventName, $eventDescription, $eventDateTime, $eventBudget, $eventsId);
     mysqli_stmt_execute($stmt);
 }
 
-// Group transactions by category and calculate total amount    
+// Group transactions by category and calculate total amount
 function groupTransactionsByCategory($transactions)
 {
     $groupedTransactions = [];
@@ -227,10 +227,10 @@ function sanitizeInput($input) {
     return htmlspecialchars($input, ENT_QUOTES, 'UTF-8');
 }
 
-function getEventInitialBudget($conn, $eventId) {
+function getEventInitialBudget($conn, $eventsId) {
     $sql = "SELECT events_budget FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
