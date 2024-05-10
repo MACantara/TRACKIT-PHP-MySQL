0bfe326d4d08f96524773ff1e6265ad15e101c74
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-10 17:43:28 +0800

    chore: Update base URL in config.php for production environment

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 // Check if the server name is localhost, if true set the base URL to your local URL, else set it to your production URL
-$base_url = ($_SERVER['SERVER_NAME'] == 'localhost') ? 'http://localhost/TRACKIT-PHP-MySQL/' : 'https://mysql-database.alwaysdata.net/';
+$base_url = ($_SERVER['SERVER_NAME'] == 'localhost') ? 'http://localhost/TRACKIT-PHP-MySQL/' : 'https://trackit.alwaysdata.net/';
 
 define('BASE_URL', $base_url);
