09577e4adced9ded8c4af8882da96c2b5757eb95
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-17 17:36:01 +0800

    chore: Update createEvent function to include event status field and handle event status in handleCreateEvent

--- includes/event.inc.php
+++ includes/event.inc.php
@@ -4,9 +4,9 @@ require_once 'error-handling-functions.inc.php';
 require_once "user-functions.inc.php";
 
 
-function createEvent($conn, $usersId, $eventName, $eventDescription, $eventDate, $eventTime, $eventBudget)
+function createEvent($conn, $usersId, $eventName, $eventStatus, $eventDescription, $eventDate, $eventTime,  $eventBudget)
 {
-    if (eventEmptyInput($eventName, $eventDescription, $eventDate, $eventTime, $eventBudget) !== false) {
+    if (eventEmptyInput($eventName, $eventStatus, $eventDescription, $eventDate, $eventTime,  $eventBudget) !== false) {
         header("location: create-event.php?error=emptyinput");
         exit();
     }
@@ -37,11 +37,14 @@ function createEvent($conn, $usersId, $eventName, $eventDescription, $eventDate,
     }
 
     $eventDateTime = $eventDate . 'T' . $eventTime . ':00';
-    $sql = "INSERT INTO events (events_name, events_description, events_date, events_budget) VALUES (?, ?, ?, ?)";
+    $sql = "INSERT INTO events (events_name, events_status, events_description, events_date, events_budget) VALUES (?, ?, ?, ?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "ssss", $eventName, $eventDescription, $eventDateTime, $eventBudget);
+    mysqli_stmt_bind_param($stmt, "sssss", $eventName, $eventStatus, $eventDescription, $eventDateTime, $eventBudget);
     mysqli_stmt_execute($stmt);
     $eventsId = mysqli_insert_id($conn);
+
+    // Fetch the departments_id associated with the usersId
+    $departmentsId = getDepartmentsIdByUsersId($conn, $usersId);
 
     // Check if the departments_id exists in the departments table
     $sql = "SELECT * FROM departments WHERE departments_id = ?";
@@ -50,7 +53,10 @@ function createEvent($conn, $usersId, $eventName, $eventDescription, $eventDate,
     mysqli_stmt_execute($stmt);
     $resultData = mysqli_stmt_get_result($stmt);
 
-    $departmentsId = getDepartmentsIdByUsersId($conn, $usersId);
+    if (mysqli_num_rows($resultData) === 0) {
+        header("location: create-event.php?error=invaliddepartmentsid");
+        exit();
+    }
 
     $sql = "INSERT INTO department_events (departments_id, events_id) VALUES (?, ?)";
     $stmt = mysqli_prepare($conn, $sql);
