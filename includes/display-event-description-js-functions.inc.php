153cccd98a81d24c0bd4d9193e7a7a17a06d1cab
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-17 19:23:30 +0800

    Refactor event description toggle function for better user experience

--- includes/display-event-description-js-functions.inc.php
+++ includes/display-event-description-js-functions.inc.php
@@ -1,17 +1,31 @@
 <script>
 function toggleEventDescription(eventId) {
-    document.getElementById('showMoreButton-' + eventId).addEventListener('click', function() {
-        document.getElementById('eventDescriptionShort-' + eventId).style.display = 'none';
-        document.getElementById('eventDescriptionFull-' + eventId).style.display = 'block';
-        document.getElementById('showMoreButton-' + eventId).style.display = 'none';
-        document.getElementById('showLessButton-' + eventId).style.display = 'block';
+    var moreText = document.getElementById('more-' + eventId);
+    var showMoreLink = document.getElementById('showMore-' + eventId);
+    var showLessLink = document.getElementById('showLess-' + eventId);
+
+    showMoreLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        var fadeOutElement = document.querySelector('#eventDescription-' + eventId + ' .fade-out');
+        fadeOutElement.classList.remove('fade-out');
+        moreText.style.display = 'inline';
+        setTimeout(function() {
+            moreText.style.opacity = '1';
+        }, 1);
+        showMoreLink.style.display = 'none';
+        showLessLink.style.display = 'inline';
     });
 
-    document.getElementById('showLessButton-' + eventId).addEventListener('click', function() {
-        document.getElementById('eventDescriptionShort-' + eventId).style.display = 'block';
-        document.getElementById('eventDescriptionFull-' + eventId).style.display = 'none';
-        document.getElementById('showMoreButton-' + eventId).style.display = 'block';
-        document.getElementById('showLessButton-' + eventId).style.display = 'none';
+    showLessLink.addEventListener('click', function(event) {
+        event.preventDefault();
+        var fadeOutElement = document.querySelector('#eventDescription-' + eventId + ' span');
+        fadeOutElement.classList.add('fade-out');
+        moreText.style.opacity = '0';
+        setTimeout(function() {
+            moreText.style.display = 'none';
+        }, 500);
+        showMoreLink.style.display = 'inline';
+        showLessLink.style.display = 'none';
     });
 }
 toggleEventDescription(<?php echo $event['events_id']; ?>);
