fd426dc0e5da1b72980d0ba7ba83457e291f9c94
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-20 22:50:45 +0800

    chore: Update event form with additional fields and labels for consistency

--- update-event.php
+++ update-event.php
@@ -28,20 +28,38 @@
             <form method="post" action="includes/update-event.inc.php">
                 <input type="hidden" name="events_id" value="<?php echo $eventsId; ?>">
 
-                <label for="event_name">Event Name:</label>
-                <input type="text" id="event_name" name="event_name" value="<?php echo $event['events_name']; ?>" required>
+                <label for="events_name">Event Name:</label>
+                <input type="text" id="events_name" name="events_name" value="<?php echo $event['events_name']; ?>" required>
 
-                <label for="event_description">Event Description:</label>
-                <textarea id="event_description" name="event_description" required><?php echo $event['events_description']; ?></textarea>
+                <label for="events_start_date">Event Start Date and Time:</label>
+                <input type="datetime-local" id="events_start_date" name="events_start_date" value="<?php echo date('Y-m-d\TH:i', strtotime($event['events_start_date'])); ?>" required>
 
-                <label for="event_date">Event Date:</label>
-                <input type="date" id="event_date" name="event_date" value="<?php echo date('Y-m-d', strtotime($event['events_date'])); ?>" required>
+                <label for="events_end_date">Event End Date and Time:</label>
+                <input type="datetime-local" id="events_end_date" name="events_end_date" value="<?php echo date('Y-m-d\TH:i', strtotime($event['events_end_date'])); ?>" required>
 
-                <label for="event_time">Event Time:</label>
-                <input type="time" id="event_time" name="event_time" value="<?php echo date('H:i', strtotime($event['events_date'])); ?>" required>
+                <label for="events_venue">Event Venue:</label>
+                <input type="text" id="events_venue" name="events_venue" value="<?php echo $event['events_venue']; ?>" required>
 
-                <label for="event_budget">Event Budget:</label>
-                <input type="number" id="event_budget" name="event_budget" value="<?php echo $event['events_budget']; ?>" required>
+                <label for="events_budget">Event Budget:</label>
+                <input type="number" id="events_budget" name="events_budget" value="<?php echo $event['events_budget']; ?>" required>
+
+                <label for="events_status">Event Status:</label>
+                <select id="events_status" name="events_status" required>
+                    <option value="" disabled>Select an Event Status</option>
+                    <option value="Upcoming" <?php echo $event['events_status'] == 'Upcoming' ? 'selected' : ''; ?>>Upcoming</option>
+                    <option value="Ongoing" <?php echo $event['events_status'] == 'Ongoing' ? 'selected' : ''; ?>>Ongoing</option>
+                    <option value="Done" <?php echo $event['events_status'] == 'Done' ? 'selected' : ''; ?>>Done</option>
+                    <option value="Canceled" <?php echo $event['events_status'] == 'Canceled' ? 'selected' : ''; ?>>Canceled</option>
+                </select>
+
+                <label for="events_description">Event Description:</label>
+                <textarea id="events_description" name="events_description" required><?php echo $event['events_description']; ?></textarea>
+
+                <label for="events_remarks">Event Remarks:</label>
+                <input type="text" id="events_remarks" name="events_remarks" value="<?php echo $event['events_remarks']; ?>" placeholder="(Optional)">
+
+                <label for="events_documentation_pictures">Event Documentation Pictures:</label>
+                <input type="file" id="events_documentation_pictures" name="events_documentation_pictures" multiple>
 
                 <div class="two-grid-column-container">
                     <a class="button margin-top-16" href="events-overview.php"><i class="bi bi-arrow-left"></i> Back</a>
