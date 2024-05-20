8e82b912282f7648e79e1d7d7958cb0900fb2a35
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-20 22:50:58 +0800

    Refactor updateEvent function to include additional event details

--- includes/update-event.inc.php
+++ includes/update-event.inc.php
@@ -5,13 +5,18 @@ require_once 'error-handling-functions.inc.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['update-event'])) {
     $eventsId = sanitizeInput($_POST['events_id']);
-    $eventsName = sanitizeInput($_POST['event_name']);
-    $eventsDescription = sanitizeInput($_POST['event_description']);
-    $eventsDate = sanitizeInput($_POST['event_date']);
-    $eventsTime = sanitizeInput($_POST['event_time']); // Retrieve event time from form data
-    $eventsBudget = sanitizeInput($_POST['event_budget']);
+    $eventsName = sanitizeInput($_POST['events_name']);
+    $eventsStartDate = sanitizeInput($_POST['events_start_date']);
+    $eventsEndDate = sanitizeInput($_POST['events_end_date']);
+    $eventsVenue = sanitizeInput($_POST['events_venue']);
+    $eventsBudget = sanitizeInput($_POST['events_budget']);
+    $eventsStatus = sanitizeInput($_POST['events_status']);
+    $eventsDescription = sanitizeInput($_POST['events_description']);
+    $eventsRemarks = sanitizeInput($_POST['events_remarks']);
+    // For file uploads, you need to handle it separately
+    // $eventsDocumentationPictures = $_FILES['events_documentation_pictures'];
 
-    updateEvent($conn, $eventsId, $eventsName, $eventsDescription, $eventsDate, $eventsTime, $eventsBudget); // Pass event time to updateEvent function
+    updateEvent($conn, $eventsId, $eventsName, $eventsStartDate, $eventsEndDate, $eventsVenue, $eventsBudget, $eventsStatus, $eventsDescription, $eventsRemarks);
 
     header("Location: ../events-overview.php?event-updated=success");
 }
