be14473ebd33b7e854d04b9342f68ecc1746cae8
Mikmikk03 <x6kvajbh@mikmikk03.anonaddy.com>
2024-05-21 18:58:49 +0800

    Refactor event-related functions and pages to include additional event details

--- includes/event-functions.inc.php
+++ includes/event-functions.inc.php
@@ -1,7 +1,8 @@
 <?php
 require_once 'event.inc.php';
 
-function handleCreateEvent($conn) {
+function handleCreateEvent($conn)
+{
     require_once "error-handling-functions.inc.php"; // Include the file containing the sanitizeInput function
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -17,27 +18,61 @@ function handleCreateEvent($conn) {
             $eventRemarks = sanitizeInput($_POST['events_remarks']);
 
             // Create the event first to get the event ID
-            $eventId = createEvent($conn, $usersId, $eventName, $eventStartDate, $eventEndDate, $eventVenue, $eventBudget, $eventStatus, $eventDescription, $eventRemarks);
+            $eventsId = createEvent($conn, $usersId, $eventName, $eventStartDate, $eventEndDate, $eventVenue, $eventBudget, $eventStatus, $eventDescription, $eventRemarks);
 
             // Loop through each array of inputs and create separate records
             foreach ($_POST['events_objectives'] as $objective) {
                 $eventObjective = sanitizeInput($objective);
-                createObjective($conn, $eventId, $eventObjective);
+                createObjective($conn, $eventsId, $eventObjective);
             }
 
             foreach ($_POST['events_problems_encountered'] as $problem) {
                 $eventProblem = sanitizeInput($problem);
-                createProblem($conn, $eventId, $eventProblem);
+                createProblem($conn, $eventsId, $eventProblem);
             }
 
             foreach ($_POST['events_actions_taken'] as $action) {
                 $eventAction = sanitizeInput($action);
-                createAction($conn, $eventId, $eventAction);
+                createAction($conn, $eventsId, $eventAction);
             }
 
             foreach ($_POST['events_recommendations'] as $recommendation) {
                 $eventRecommendation = sanitizeInput($recommendation);
-                createRecommendation($conn, $eventId, $eventRecommendation);
+                createRecommendation($conn, $eventsId, $eventRecommendation);
+            }
+
+            // Process the uploaded files
+            if (isset($_FILES['events_documentation_pictures'])) {
+                require_once 'event.inc.php';
+
+                $pictures = $_FILES['events_documentation_pictures'];
+
+                // Prepare an array of pictures
+                $picturesArray = [];
+
+                // Check if multiple files are uploaded
+                if (is_array($pictures['name'])) {
+                    // Get the count of uploaded files
+                    $fileCount = count($pictures['name']);
+                } else {
+                    // Only one file is uploaded
+                    $fileCount = 1;
+                    $pictures['name'] = array($pictures['name']);
+                    $pictures['type'] = array($pictures['type']);
+                    $pictures['tmp_name'] = array($pictures['tmp_name']);
+                    $pictures['error'] = array($pictures['error']);
+                    $pictures['size'] = array($pictures['size']);
+                }
+
+                $fileKeys = array_keys($pictures);
+
+                for ($i = 0; $i < $fileCount; $i++) {
+                    foreach ($fileKeys as $key) {
+                        $picturesArray[$i][$key] = $pictures[$key][$i];
+                    }
+                }
+
+                storeEventDocumentationPicture($conn, $eventsId, $picturesArray);
             }
 
             header("Location: events-overview.php?create-event=success");
@@ -47,7 +82,8 @@ function handleCreateEvent($conn) {
     }
 }
 
-function getEvent($conn, $eventsId) {
+function getEvent($conn, $eventsId)
+{
     $sql = "SELECT * FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $eventsId);
@@ -56,7 +92,8 @@ function getEvent($conn, $eventsId) {
     return mysqli_fetch_assoc($result);
 }
 
-function getEventName($conn, $eventsId) {
+function getEventName($conn, $eventsId)
+{
     $sql = "SELECT events_name FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $eventsId);
@@ -66,7 +103,8 @@ function getEventName($conn, $eventsId) {
     return $row['events_name'];
 }
 
-function getTransactions($conn, $eventsId, $sort = 'DESC', $filterDays = null, $transactionType = null, $startFrom = 0, $recordsPerPage = 0) {
+function getTransactions($conn, $eventsId, $sort = 'DESC', $filterDays = null, $transactionType = null, $startFrom = 0, $recordsPerPage = 0)
+{
     $sql = "SELECT * FROM transaction_history WHERE events_id = ?";
     $params = [$eventsId];
     $types = "i";
@@ -95,7 +133,8 @@ function getTransactions($conn, $eventsId, $sort = 'DESC', $filterDays = null, $
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function getUserEvents($conn, $usersId, $sortOrder = 'DESC') {
+function getUserEvents($conn, $usersId, $sortOrder = 'DESC')
+{
     $sql = "SELECT events.events_id, events.events_status, events.events_name, events.events_description, events.events_start_date, events.events_end_date, events.events_venue, events.events_budget, events.events_remarks 
             FROM events 
             JOIN department_events ON events.events_id = department_events.events_id 
@@ -109,7 +148,8 @@ function getUserEvents($conn, $usersId, $sortOrder = 'DESC') {
     return mysqli_fetch_all($result, MYSQLI_ASSOC);
 }
 
-function getCategories($conn, $eventsId) {
+function getCategories($conn, $eventsId)
+{
     $sql = "SELECT transaction_category FROM transaction_history WHERE events_id = ? GROUP BY transaction_category;"; // adjust the table and column names as needed
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -123,7 +163,8 @@ function getCategories($conn, $eventsId) {
     }
 }
 
-function getTotalEventExpenses($conn, $eventsId) {
+function getTotalEventExpenses($conn, $eventsId)
+{
     $sql = "SELECT SUM(transaction_total) AS total_expenses FROM transaction_history WHERE events_id = ? AND transaction_type = 'expense'";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $eventsId);
@@ -133,7 +174,8 @@ function getTotalEventExpenses($conn, $eventsId) {
     return $row['total_expenses'];
 }
 
-function getTotalEventIncome($conn, $eventsId) {
+function getTotalEventIncome($conn, $eventsId)
+{
     $sql = "SELECT SUM(transaction_total) AS total_income FROM transaction_history WHERE events_id = ? AND transaction_type = 'income'";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $eventsId);
@@ -142,8 +184,9 @@ function getTotalEventIncome($conn, $eventsId) {
     $row = mysqli_fetch_assoc($result);
     return $row['total_income'];
 }
-    
-function getEventRemainingBudget($conn, $eventsId) {
+
+function getEventRemainingBudget($conn, $eventsId)
+{
     $event = getEvent($conn, $eventsId);
     $expenses = getTotalEventExpenses($conn, $eventsId);
     $income = getTotalEventIncome($conn, $eventsId);
@@ -151,7 +194,8 @@ function getEventRemainingBudget($conn, $eventsId) {
     return $remainingBudget;
 }
 
-function getEventManagers($conn, $eventsId) {
+function getEventManagers($conn, $eventsId)
+{
     $sql = "SELECT users.users_last_name, users.users_first_name FROM department_users
             INNER JOIN users ON department_users.users_id = users.users_id
             INNER JOIN department_events ON department_users.departments_id = department_events.departments_id
@@ -193,7 +237,8 @@ function getEventExpenses($conn, $eventsId)
     return $expenses;
 }
 
-function getEventIncomes($conn, $eventsId) {
+function getEventIncomes($conn, $eventsId)
+{
     $sql = "SELECT transaction_category, SUM(transaction_total) as transaction_total
             FROM transaction_history
             WHERE events_id = ? AND transaction_type = 'Income'
@@ -215,7 +260,8 @@ function getEventIncomes($conn, $eventsId) {
     return $income;
 }
 
-function deleteEvent($conn, $eventsId) {
+function deleteEvent($conn, $eventsId)
+{
     $sql = "DELETE FROM events WHERE events_id = ?";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
@@ -226,14 +272,16 @@ function deleteEvent($conn, $eventsId) {
     return true;
 }
 
-function updateEvent($conn, $eventsId, $eventsName, $eventsStartDate, $eventsEndDate, $eventsVenue, $eventsBudget, $eventsStatus, $eventsDescription, $eventsRemarks) {
+function updateEvent($conn, $eventsId, $eventsName, $eventsStartDate, $eventsEndDate, $eventsVenue, $eventsBudget, $eventsStatus, $eventsDescription, $eventsRemarks)
+{
     $sql = "UPDATE events SET events_name = ?, events_start_date = ?, events_end_date = ?, events_venue = ?, events_budget = ?, events_status = ?, events_description = ?, events_remarks = ? WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "ssssisssi", $eventsName, $eventsStartDate, $eventsEndDate, $eventsVenue, $eventsBudget, $eventsStatus, $eventsDescription, $eventsRemarks, $eventsId);
     mysqli_stmt_execute($stmt);
 }
 
-function updateOrInsertRecord($conn, $table, $id, $value, $eventId) {
+function updateOrInsertRecord($conn, $table, $id, $value, $eventsId)
+{
     // If the value is empty, set it to NULL
     if (empty($value)) {
         $value = NULL;
@@ -266,8 +314,52 @@ function updateOrInsertRecord($conn, $table, $id, $value, $eventId) {
         $junctionTable = "event_$table";
         $sql = "INSERT INTO $junctionTable (events_id, {$table}_id) VALUES (?, ?)";
         $stmt = mysqli_prepare($conn, $sql);
-        mysqli_stmt_bind_param($stmt, "ii", $eventId, $insertedId);
+        mysqli_stmt_bind_param($stmt, "ii", $eventsId, $insertedId);
         mysqli_stmt_execute($stmt);
+    }
+}
+
+
+function updateEventDocumentationPictures($conn, $eventsId, $pictures) {
+    foreach ($pictures as $picture) {
+        // Check for upload errors
+        if ($picture['error'] !== UPLOAD_ERR_OK) {
+            // Handle error
+            continue;
+        }
+
+        // Read the file content
+        $pictureContent = file_get_contents($picture['tmp_name']);
+
+        // Check if the picture already exists
+        $sql = "SELECT * FROM documentation_pictures WHERE documentation_pictures_id = ?";
+        $stmt = mysqli_prepare($conn, $sql);
+        mysqli_stmt_bind_param($stmt, "i", $eventsId);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        if (mysqli_num_rows($result) > 0) {
+            // The picture exists, update it
+            $sql = "UPDATE documentation_pictures SET documentation_pictures_item = ? WHERE documentation_pictures_id = ?";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "si", $pictureContent, $eventsId);
+            mysqli_stmt_execute($stmt);
+        } else {
+            // The picture doesn't exist, insert it
+            $sql = "INSERT INTO documentation_pictures (documentation_pictures_item) VALUES (?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "s", $pictureContent);
+            mysqli_stmt_execute($stmt);
+
+            // Get the ID of the inserted picture
+            $pictureId = mysqli_insert_id($conn);
+
+            // Insert a record into the event_documentation_pictures table
+            $sql = "INSERT INTO event_documentation_pictures (events_id, documentation_pictures_id) VALUES (?, ?)";
+            $stmt = mysqli_prepare($conn, $sql);
+            mysqli_stmt_bind_param($stmt, "ii", $eventsId, $pictureId);
+            mysqli_stmt_execute($stmt);
+        }
     }
 }
 
@@ -286,7 +378,8 @@ function groupTransactionsByCategory($transactions)
     return $groupedTransactions;
 }
 
-function groupOtherCategories($groupedTransactions, $limit) {
+function groupOtherCategories($groupedTransactions, $limit)
+{
     $topCategories = array_slice($groupedTransactions, 0, $limit, true);
     if (count($groupedTransactions) > $limit) {
         $topCategories['Other'] = array_sum(array_slice($groupedTransactions, $limit));
@@ -294,7 +387,8 @@ function groupOtherCategories($groupedTransactions, $limit) {
     return $topCategories;
 }
 
-function getEventInitialBudget($conn, $eventsId) {
+function getEventInitialBudget($conn, $eventsId)
+{
     $sql = "SELECT events_budget FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
     mysqli_stmt_bind_param($stmt, "i", $eventsId);
@@ -304,7 +398,8 @@ function getEventInitialBudget($conn, $eventsId) {
     return $row['events_budget'];
 }
 
-function calculateBudget($totalExpenses, $remainingBudget, $transactions, $row, $eventsId, $conn) {
+function calculateBudget($totalExpenses, $remainingBudget, $transactions, $row, $eventsId, $conn)
+{
     // If remaining budget is less than 0, set it to 0
     if ($remainingBudget < 0) {
         $remainingBudget = 0;
@@ -329,16 +424,18 @@ function calculateBudget($totalExpenses, $remainingBudget, $transactions, $row, 
     return array($expensesWithinBudget, $expensesOverBudget, $remainingBudget);
 }
 
-function limit_words($string, $word_limit) {
-    $words = explode(" ",$string);
-    $limited_string = implode(" ",array_splice($words,0,$word_limit));
+function limit_words($string, $word_limit)
+{
+    $words = explode(" ", $string);
+    $limited_string = implode(" ", array_splice($words, 0, $word_limit));
     return (str_word_count($string) > $word_limit) ? $limited_string : $string;
 }
 
-function getEventStatus($conn, $eventId) {
+function getEventStatus($conn, $eventsId)
+{
     $sql = "SELECT events_status FROM events WHERE events_id = ?";
     $stmt = mysqli_prepare($conn, $sql);
-    mysqli_stmt_bind_param($stmt, "i", $eventId);
+    mysqli_stmt_bind_param($stmt, "i", $eventsId);
     mysqli_stmt_execute($stmt);
     $result = mysqli_stmt_get_result($stmt);
     $row = mysqli_fetch_assoc($result);
